847d29c197b46c10c4395c37771cf2d9b7c2f20f
ksubi666 <ksubi666@gmail.com>
2024-08-09 18:04:16 +0800

    m

--- Next.js/FE-expense-tracker/src/pages/index.js
+++ Next.js/FE-expense-tracker/src/pages/index.js
@@ -4,18 +4,5 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [res, setRes] = useState("hehe");
-  // useEffect(() => {
-  //   axios.get("http://localhost:8000/categories").then((response) => {
-  //     console.log("success");
-  //     console.log(response.data);
-  //     setRes(response.data);
-  //   });
-  // }, []);
-
-  return (
-    <main className="flex justify-center items-center">
-      <Button variant="outline">{res}</Button>
-    </main>
-  );
+  return <main className="flex justify-center items-center"></main>;
 }
--- Next.js/FE-expense-tracker/src/pages/sign-up/index.jsx
+++ Next.js/FE-expense-tracker/src/pages/sign-up/index.jsx
@@ -2,20 +2,26 @@ import { LogoIcon } from "@/components/icon/LogoIcon";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import React from "react";
-const SigninPage = () => {
+
+const SignupPage = () => {
   return (
     <div className="grid grid-cols-2 w-full h-screen">
       <div className="flex flex-col justify-center items-center w-[384px] gap-10">
         <LogoIcon />
         <div className="flex flex-col items-center gap-2">
           <h1 className="text-2xl leading-8 font-semibold text-[#0F172A]">
-            Welcome Back
+            Create Geld account
           </h1>
           <p className="text-base font-normal leading-6 text-[#334155]">
-            Welcome back, Please enter your details
+            Sign up below to create your Wallet account
           </p>
         </div>
         <div className="w-full flex flex-col gap-4">
+          <Input
+            className="p-4 items-center bg-[#F3F4F6] text-[#A3A3A3] h-12"
+            type="text"
+            placeholder="Name"
+          />
           <Input
             className="p-4 items-center bg-[#F3F4F6] text-[#A3A3A3] h-12"
             type="email"
@@ -26,13 +32,18 @@ const SigninPage = () => {
             type="password"
             placeholder="Password"
           />
+          <Input
+            className="p-4 items-center bg-[#F3F4F6] text-[#A3A3A3] h-12"
+            type="email"
+            placeholder="Re-password"
+          />
           <Button className="w-full rounded-3xl bg-[#0166FF] h-12 text-xl leading-7 font-normal">
-            Log in
+            Sign up
           </Button>
         </div>
         <div className="flex items-center">
-          <p>Don’t have account?</p>
-          <Button className="bg-white text-[#0166FF]">Sign up</Button>
+          <p>Already have account?</p>
+          <Button className="bg-white text-[#0166FF]">Login</Button>
         </div>
       </div>
       <div className="bg-[#0166FF]"></div>
@@ -40,4 +51,4 @@ const SigninPage = () => {
   );
 };
 
-export default SigninPage;
+export default SignupPage;
